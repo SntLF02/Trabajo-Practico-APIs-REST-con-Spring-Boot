91f648e8428fca92cf0096872edf20830f28c911
Santiago <20.santi.lopez02@gmail.com>
2025-11-24 00:55:52 -0300

    Repository y Service

--- productos-api/src/main/java/com/utn/productos/repository/ProductoRepository.java
+++ productos-api/src/main/java/com/utn/productos/repository/ProductoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    List<Producto> findByCategoria(Categoria categoria);
-}
 
+    List<Producto> findByCategoria(Categoria categoria);
+
+}
--- productos-api/src/main/java/com/utn/productos/service/ProductoService.java
+++ productos-api/src/main/java/com/utn/productos/service/ProductoService.java
@@ -58,4 +58,3 @@ public class ProductoService {
         productoRepository.deleteById(id);
     }
 }
-
